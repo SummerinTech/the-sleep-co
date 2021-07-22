e3fa0e1085ac3dbc6833bb63052d154c52309111
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-07-22 15:37:18 -0700

    added a popup gallery

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+$( document ).ready(function() {
+
 $(".hamburger-icon").click(function () {
 	$(".mobile-nav").fadeToggle(100);
+});
+
+});
+
+$(document).ready(function() {
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
 });
